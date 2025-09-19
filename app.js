c92665c635a9c9f2912dab706946a9a2118257d5
zosarr <74683924+zosarr@users.noreply.github.com>
2025-09-19 20:41:29 +0200

    app.js

--- app.js
+++ app.js
@@ -294,7 +294,7 @@ window.addEventListener('DOMContentLoaded', () => {
   function b64ToAb(b64){
     const bin = atob(b64);
     const bytes = new Uint8Array(bin.length);
-    for (let i=0;i<bin.length;i++) bytes[i]=bin.charCodeAt(0);
+    for (let i=0;i<bin.length;i++) bytes[i] = bin.charCodeAt(i);
     return bytes.buffer;
   }
 
